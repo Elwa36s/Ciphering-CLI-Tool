0baa7abdc0e91e2bed39cae7508b0a1e11b018ef
Elwa36s <24dds08@gmail.com>
2021-11-14 22:59:02 +0300

    add output check

--- consoleStreams.js
+++ consoleStreams.js
@@ -4,6 +4,13 @@ const {selectCifer} = require('./selectCifer');
 module.exports.doConfig = (config) => {
   const {i, o, c} = config;
 
+  if (o){
+    fs.createReadStream(o).on('error', (error) => {
+      process.stderr._write("File doesn't exist or wrong path");
+      process.exit(3);
+    })
+  }
+
   const reader = i ? fs.createReadStream(i, 'utf-8') : process.stdin;
   const writer = o ? fs.createWriteStream(o, { flags: 'a', encoding: 'utf-8'}) : process.stdout;
 
