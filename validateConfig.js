89968aea033379a239e3f045e053b9e8f9ddaa25
Elwa36s <24dds08@gmail.com>
2021-11-14 22:54:03 +0300

    some fixes

--- validateConfig.js
+++ validateConfig.js
@@ -7,7 +7,7 @@ module.exports.validateConfig = (config) => {
         config.split('-').forEach(element => {
             if (element.length < 2 && element !== 'A') throw 'Incorrect config. Only "A" could be without number key.'
             if (element.length > 2) throw 'Incorrect config. Each step should contain 2 symbols or 1 in case of using "A".'
-            if (element.length === 2 && ((element[0] !== 'C') && (element[0] !== 'R'))) throw 'Incorrect config. "C", "R" options should contain key.'
+            if (element.length === 2 && ((element[0] !== 'C') && (element[0] !== 'R'))) throw 'Incorrect config. Only "C", "R" options should contain key.'
         });
     } catch(error) {
         process.stderr._write(error)
